118a10fdd64351b52b894b17c8586fde1a7d1786
Arjen Simons <arjen.simons@gmail.com>
2019-10-02 15:57:37 +0200

    Revert "Merge remote-tracking branch 'origin/feature-architecture' into feature-architecture"
    This reverts commit dd9ca1ed1e096a900c8602e9ef2cb1664a9c10c8.

---
+++ Vittles/app/src/main/java/com/example/vittles/productlist/ProductsActivity.kt
@@ -0,0 +1,134 @@
+package com.example.vittles.productlist
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.domain.model.Product
+import com.example.vittles.R
+import com.example.vittles.VittlesApp
+import com.example.vittles.mvp.BaseActivity
+import com.example.vittles.productadd.AddProductActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import javax.inject.Inject
+
+/**
+ * Activity class for the main activity. This is the activity that shows the list of products.
+ *
+ * @author Arjen Simons
+ * @author Jeroen Flietstra
+ * @author Jan-Willem van Bremen
+ */
+class ProductsActivity : BaseActivity() {
+
+    @Inject lateinit var presenter: ProductsPresenter
+
+    private var products = arrayListOf<Product>()
+    private val productAdapter = ProductAdapter(products)
+
+    /**
+     * Called when the ProductsActivity is created.
+     *
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.inject
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        with(presenter) {
+            start(this@ProductsActivity)
+        }
+        initViews()
+    }
+
+     override fun injectDependencies() {
+        DaggerProductsComponent.builder()
+            .appComponent(VittlesApp.component)
+            .productsModule(ProductsModule())
+            .build()
+            .inject(this)
+    }
+
+    /**
+     * Called when the mainActivity starts.
+     * Re-populates the RecyclerView.
+     */
+    override fun onStart() {
+        super.onStart()
+        populateRecyclerView()
+    }
+
+    /**
+     * Called when the option menu is created.
+     *.
+     */
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    /**
+     * Handles action bar item clicks.
+     *
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    /**
+     * Initializes the RecyclerView.
+     */
+    private fun initViews(){
+        fab.setOnClickListener { onAddButtonClick() }
+
+        rvProducts.layoutManager =
+            LinearLayoutManager(this@ProductsActivity, RecyclerView.VERTICAL, false)
+        rvProducts.adapter = productAdapter
+    }
+
+    /**
+     * Called when the add button is clicked.
+     * It starts the addProduct activity.
+     */
+    private fun onAddButtonClick() {
+        val addProductActivityIntent = Intent(
+            this,
+            AddProductActivity::class.java
+        )
+        startActivity(addProductActivityIntent)
+    }
+
+    /**
+     * Populates the RecyclerView with items from the local DataBase.
+     */
+    private fun populateRecyclerView(){
+        products.clear()
+        presenter.loadProducts()
+    }
+
+    /**
+     * When products are loaded, this method will add the products to the product list.
+     *
+     * @param products Products to be added to the product list.
+     */
+    fun onProductsLoadSucceed(products: List<Product>) {
+        this.products.addAll(products)
+        presenter.loadIndicationColors(this.products)
+        productAdapter.notifyDataSetChanged()
+    }
+
+    /**
+     * If the products could not be loaded, this method will handle the error.
+     *
+     */
+    fun onProductsLoadFail() {
+        println("FAIL")
+    }
+}
