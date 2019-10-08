628b7ca1e78a51409f32560d196a00b2a5c716f6
Fethi Tewelde <fithikidane@gmail.com>
2019-10-08 14:38:02 +0200

    Solved Merge conflict

--- Vittles/app/src/main/java/com/example/vittles/productlist/ProductsActivity.kt
+++ Vittles/app/src/main/java/com/example/vittles/productlist/ProductsActivity.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.view.View
 import android.widget.SearchView
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
