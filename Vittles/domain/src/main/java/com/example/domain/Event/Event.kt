9e8f2f5712edbf9e5d6fa96d30241af6123cbee3
Arjen Simons <arjen.simons@gmail.com>
2019-10-16 10:13:41 +0200

    Removed unnecessary documentation

--- Vittles/domain/src/main/java/com/example/domain/Event/Event.kt
+++ Vittles/domain/src/main/java/com/example/domain/Event/Event.kt
@@ -7,34 +7,10 @@ package com.example.domain.Event
  * @author Arjen Simons
  *
  * @param T Data type to pass into the event.
- *
- * @property handlers A list of units (in the form of lambda expressions) subscribed to an event.
  */
 class Event<T> {
-    /**
-     * A list of units (in the form of lambda expressions) subscribed to an event.
-     *
-     */
     private val handlers = arrayListOf<(Event<T>.(T) -> Unit)>()
-
-    /**
-     * Used to subscribe to an event.
-     *
-     * @param handler The unit that should be subscribed.
-     */
     operator fun plusAssign(handler: Event<T>.(T) -> Unit) { handlers.add(handler) }
-
-    /**
-     * Used to unsubscribe from an event.
-     *
-     * @param handler The handler that should be unsubscribed.
-     */
     operator fun minusAssign(handler: Event<T>.(T) -> Unit) { handlers.remove(handler) }
-
-    /**
-     * Used to invoke the event (makes it so all the subscribed handlers execute.
-     *
-     * @param value The value that is passed into the event
-     */
     operator fun invoke(value: T) { for (handler in handlers) handler(value) }
 }
