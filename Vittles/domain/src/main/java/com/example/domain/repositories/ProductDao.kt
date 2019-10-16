17033e86ddb61ccada45f6fdf884f09efe82493d
JeroenFlietstra <jeroenflietstra@live.nl>
2019-10-16 14:06:42 +0200

    Removed unnecessary interfaces.

--- Vittles/domain/src/main/java/com/example/domain/repositories/ProductDao.kt
+++
@@ -1,9 +0,0 @@
-package com.example.domain.repositories
-
-/**
- * Marker interface to satisfy Generics for {@link ProductsRepository}.
- *
- * @author Jeroen Flietstra
- * @author Arjen Simons
- */
-interface ProductDao
--- Vittles/domain/src/main/java/com/example/domain/repositories/ProductModelMapper.kt
+++
@@ -1,9 +0,0 @@
-package com.example.domain.repositories
-
-/**
- * Data mapper used to map database entities to model classes and vice versa.
- */
-interface ProductModelMapper<E, M> {
-    fun fromEntity(from: E): M
-    fun toEntity(from: M): E
-}
