a183a56a6d82297847df1998e041d7e60f0a1ea6
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2019-11-05 17:16:09 +0100

    Added text methods to scannerContract

--- Vittles/app/src/main/java/com/example/vittles/scanning/ScannerContract.kt
+++ Vittles/app/src/main/java/com/example/vittles/scanning/ScannerContract.kt
@@ -3,6 +3,7 @@ package com.example.vittles.scanning
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.Preview
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode
+import com.google.firebase.ml.vision.text.FirebaseVisionText
 
 /**
  * MVP Contract for scanning products.
@@ -16,6 +17,8 @@ interface ScannerContract {
         fun onAddVittleButtonClick()
         fun onBarcodeScanned(barcodes: List<FirebaseVisionBarcode>)
         fun onBarcodeNotFound()
+        fun onTextScanned(text: String)
+        fun onTextNotFound()
         fun onNoPermissionGranted()
         fun onBackButtonClick(): Boolean
     }
