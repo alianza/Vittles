e6a4d2b54a21ea646450862e51ffa875c7e4b472
sarrah897 <50952828+sarrah897@users.noreply.github.com>
2019-11-10 14:00:36 +0100

    added get use cases

---
+++ Vittles/domain/src/main/java/com/example/domain/wasteReport/GetCountExpiredProducts.kt
@@ -0,0 +1,21 @@
+package com.example.domain.wasteReport
+
+import com.example.domain.repositories.WasteReportRepository
+import io.reactivex.Single
+import org.joda.time.DateTime
+import javax.inject.Inject
+
+/**
+ * This class handles te business logic of getting waste report.
+ *
+ * @author Sarah Lange
+ *
+ * @property repository The WasteReportRepository.
+ */
+class GetCountExpiredProducts @Inject constructor(private val repository: WasteReportRepository) {
+
+
+    operator fun invoke(date: DateTime): Single<Int> = repository.getCountExpiredProducts(date.millis)
+
+
+}
