ea2c15c5304a9d8610c7e1ec1114659f3fafa348
JeroenFlietstra <jeroenflietstra@live.nl>
2019-11-11 14:40:05 +0100

    Added suppressing annotation

--- Vittles/data/src/main/java/com/example/data/retrofit/tesco/TescoResult.kt
+++ Vittles/data/src/main/java/com/example/data/retrofit/tesco/TescoResult.kt
@@ -2,6 +2,7 @@ package com.example.data.retrofit.tesco
 
 import com.google.gson.annotations.SerializedName
 
+@Suppress("ArrayInDataClass")
 data class TescoResult(
     @SerializedName("products") var products: Array<TescoProduct>?
 )
