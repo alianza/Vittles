2d8bbceede262873c1fcbc2d1b43d6ba53abbd1d
M.M.A. van Spronsen <marc.van.spronsen@hva.nl>
2019-11-13 15:02:59 +0100

    Added logic to accept multiple formats

--- Vittles/app/src/main/java/com/example/vittles/services/scanner/DateFormatterService.kt
+++ Vittles/app/src/main/java/com/example/vittles/services/scanner/DateFormatterService.kt
@@ -8,8 +8,13 @@ import java.util.*
 object DateFormatterService {
 
     private val regex = Regex("^([0-9&/]*)\$")
+    private val shortNumberRegex = Regex("^([0-9]*)")
+    private val shortCharRegex = Regex("^([a-z]*)")
     private val charFormat = DateTimeFormat.forPattern("dd/MMM/yyyy").withLocale(Locale.ROOT)
     val numberFormat: DateTimeFormatter = DateTimeFormat.forPattern("dd/MM/yyyy").withLocale(Locale.ROOT)
+    private var replacedText = "Place Holder Text"
+    lateinit var formatter: DateTimeFormatter
+    lateinit var checkMonth: MatchResult
 
     /**
      * sets the retrieved text in the correct date format and returns the date
@@ -17,18 +22,48 @@ object DateFormatterService {
      * @param text The text that has been retrieved from the camera
      */
     fun expirationDateFormatter(text: String): DateTime? {
-        var replacedText = text.replace('-', '/').replace(':', '/').replace('.', '/').replace(' ', '/')
+        replacedText = text.replace('-', '/').replace(':', '/').replace('.', '/').replace(' ', '/')
         replacedText = replacedText.replace("okt", "oct").replace("mei", "may").replace("mrt", "mar")
 
-        val formatter = if (replacedText.matches(regex)) {
-            numberFormat
+
+        if (replacedText.matches(regex)){
+            if (replacedText.length < 8) {
+                checkMonth = shortNumberRegex.find(replacedText)!!
+                addDayOfMonth(checkMonth)
+            }
+            formatter = numberFormat
         } else {
-            charFormat
+            if (replacedText.length < 9) {
+                checkMonth = shortCharRegex.find(replacedText)!!
+                addDayOfMonth(checkMonth)
+            }
+            formatter = charFormat
         }
+
+//        val formatter = if (replacedText.matches(regex)) {
+//            numberFormat
+//        } else {
+//            charFormat
+//        }
 
         var expirationDate = formatter.parseDateTime(replacedText)
         expirationDate = expirationDate.withCenturyOfEra(DateTime.now().centuryOfEra)
 
         return expirationDate
+    }
+
+    fun addDayOfMonth(monthToCheck: MatchResult?) {
+        if (monthToCheck !== null) {
+            if (checkMonth.value == "01" || checkMonth.value == "03" || checkMonth.value == "05" || checkMonth.value == "07" || checkMonth.value == "08" || checkMonth.value == "10" || checkMonth.value == "12" ||
+                checkMonth.value == "jan" || checkMonth.value == "mar" || checkMonth.value == "may" || checkMonth.value == "jul" || checkMonth.value == "aug" || checkMonth.value == "oct" || checkMonth.value == "dec") {
+                replacedText = "31/" + replacedText
+            } else if (checkMonth.value == "04" || checkMonth.value == "06" || checkMonth.value == "09" || checkMonth.value == "11" ||
+                checkMonth.value == "apr" || checkMonth.value == "jun" || checkMonth.value == "sep" || checkMonth.value == "nov") {
+                replacedText = "30/" + replacedText
+            } else {
+                replacedText = "28/" + replacedText
+                println("newText: " + replacedText)
+            }
+        }
     }
 }
