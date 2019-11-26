9b8785cc2e27c27af7e191a8710b51568b1a38df
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2019-11-26 16:44:24 +0100

    Fixed more merge stuff

--- Vittles/data/src/androidTest/java/com/example/data/ExampleInstrumentedTest.kt
+++ Vittles/data/src/androidTest/java/com/example/data/ExampleInstrumentedTest.kt
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith
 import org.junit.Assert.*
 
 /**
- * Instrumented fragment_settings, which will execute on an Android device.
+ * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
@@ -17,8 +17,8 @@ import org.junit.Assert.*
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
-        // Context of the app under fragment_settings.
+        // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.data.fragment_settings", appContext.packageName)
+        assertEquals("com.example.data.test", appContext.packageName)
     }
 }
